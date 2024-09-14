7e65ef2e7c7ce12ef45db8695657b9b3a706b797
quintus <quentin@harnay.eu>
2024-09-14 20:19:47 +0200

    Initial Commit

---
+++ bike_cockpit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bike_cockpit
+//
+//  Created by Célia Giraud on 14/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bike_cockpit/bike_cockpitApp.swift
@@ -0,0 +1,17 @@
+//
+//  bike_cockpitApp.swift
+//  bike_cockpit
+//
+//  Created by Célia Giraud on 14/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct bike_cockpitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
