1e2d37b723ff0b741cf33d6c6c6567261c8ebdb6
quintus <quentin@harnay.eu>
2024-09-15 13:21:41 +0200

    Feat : add settings page

---
+++ bike_cockpit/Utility/ColorExtension.swift
@@ -0,0 +1,8 @@
+//
+//  ColorExtension.swift
+//  bike_cockpit
+//
+//  Created by Célia Giraud on 15/09/2024.
+//
+
+import Foundation
---
+++ bike_cockpit/Views/SettingsView.swift
@@ -0,0 +1,8 @@
+//
+//  SettingsView.swift
+//  bike_cockpit
+//
+//  Created by Célia Giraud on 15/09/2024.
+//
+
+import Foundation
